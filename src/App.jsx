e30d36a274a0ee950ad618ce5938233619211c88
Srivardhan <srivardhanr9@gmail.com>
2024-12-30 17:21:16 +0530

    Work in progress on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./layout.scss"
 import "./responsive.scss"
 import Navbar from "./components/navbar/NavBar";
+import HomePage from "./routes/homepage/HomePage";
 
 function App() {
   return (
     <div className = "layout">
+      <div className="navbar">
         <Navbar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
+        
     </div>
   )
 }
