6d279bd26a417d7da6be0100668d391158215a40
Srivardhan <srivardhanr9@gmail.com>
2025-01-03 16:59:42 +0530

    Worked on SearchBar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import "./searchbar.scss";
 
+const types = ["buy","rent"];
+
 function SearchBar(){
+    const [query,setQuery] = useState({
+        type:"buy",
+        location:"",
+        minPrice:0,
+        maxPrice:0
+    });
+    const switchType = (val) =>{
+        setQuery((prev)=>({...prev,type:val}));
+    }
+
     return (
-    <div className="">
+    <div className="searchbar">
         <div className="type">
-            <button>Buy</button>
-            <button>Sell</button>
+            {types.map((type)=>(
+                <button
+                    key = {type}
+                    onClick={()=>switchType(type)}
+                    className={query.type==type? "active":""}
+                >
+                    {type}
+                </button>
+            ))}
         </div>
         <form>
             <input 
